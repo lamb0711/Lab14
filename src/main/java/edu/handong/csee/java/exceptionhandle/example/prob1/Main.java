1c951a1a474b7e73e32d502620e6b729ae367808
Yang sujin <21500394@handong.edu>
2018-04-30 11:09:27 +0900

    import scanner class and instantiation it

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/Main.java
@@ -1,10 +1,11 @@
 package edu.handong.csee.java.exceptionhandle.example.prob1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
 	}
 
 }
