0b28754577aa395f1b661d0e68f06f3dd01528c6
Yang sujin <21500394@handong.edu>
2018-04-30 11:02:18 +0900

    change constructor name

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
@@ -3,7 +3,7 @@ package edu.handong.csee.java.exceptionhandle.example.prob1;
 public class SpeedLimitter {
 	private int speed = 0;
 	private int limit = 0;
-	public speedLimitter(int limit, int speed) {
+	public SpeedLimitter(int limit, int speed) {
 		this.limit = limit;
 		this.speed = speed;
 	}
