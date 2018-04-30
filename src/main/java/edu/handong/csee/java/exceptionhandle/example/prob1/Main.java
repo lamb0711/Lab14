6bfc63926ebf9f5fd4e36dc7f3e15c6ca14fea97
Yang sujin <21500394@handong.edu>
2018-04-30 11:10:04 +0900

    use random class and instantiation it

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/Main.java
@@ -1,11 +1,13 @@
 package edu.handong.csee.java.exceptionhandle.example.prob1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+		Random rand = new Random();
 	}
 
 }
