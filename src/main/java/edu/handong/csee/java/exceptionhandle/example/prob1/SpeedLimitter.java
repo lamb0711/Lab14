753c7a5ac6c832de08be395effb8b2b07398cd52
Yang sujin <21500394@handong.edu>
2018-04-30 10:59:48 +0900

    make speedLimitter method

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
@@ -1,5 +1,10 @@
 package edu.handong.csee.java.exceptionhandle.example.prob1;
 
 public class SpeedLimitter {
+	private int speed = 0;
+	private int limit = 0;
+	public speedLimitter(int limit, int speed) {
+		
+	}
 
 }
