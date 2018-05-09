6d6584d290ae1684d10ce0462e7968d74bc63a42
Yang sujin <21500394@handong.edu>
2018-05-10 01:58:05 +0900

    create prob3 package and method

---
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerMain.java
@@ -0,0 +1,10 @@
+package edu.handong.csee.java.exceptionhandle.example.prob3;
+
+public class PowerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
