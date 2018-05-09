11d4399423585a399ff59c278ab7f8010b7cb071
Yang sujin <21500394@handong.edu>
2018-05-10 01:55:35 +0900

    add line comments and arrange prob2

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/In_exception.java
@@ -1,35 +1,36 @@
-package edu.handong.csee.java.exceptionhandle.example.prob2;
+package edu.handong.csee.java.exceptionhandle.example.prob2;//package name
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.InputMismatchException;//use InputMismatchException class
+import java.util.Scanner;//use scanner class
 
-public class In_exception {
-	private int x=0, y=0;
-	
-	public In_exception() {
+public class In_exception {//class name
+	private int x=0, y=0;//declare and initialize x,y variable
+
+	public In_exception() {//constructor
 	}
-	public void distinguishError() {
-		
+
+	public void distinguishError() {//distinguishError method
+
 		try {
-			Scanner keyboard = new Scanner(System.in);
-			
-			System.out.print("x: ");
-			x = keyboard.nextInt();
-			System.out.print("y: ");
-			y = keyboard.nextInt();
-			
-			System.out.println(this.x/this.y);
+			Scanner keyboard = new Scanner(System.in);//use scanner class with keyboard
+
+			System.out.print("x: ");//print message for input x data
+			x = keyboard.nextInt();//save input data to x
+			System.out.print("y: ");//print message for input y data
+			y = keyboard.nextInt();//save input data to y
+
+			System.out.println(this.x/this.y);//print divided result
 		}
-		catch(ArithmeticException e) {
-			System.out.println("java.lang.ArithmeticExcetion: "+e.getMessage());
+		catch(ArithmeticException e) {//if y is 0, implement this 
+			System.out.println("java.lang.ArithmeticExcetion: "+e.getMessage());//print out error message with Exception String
 		}
-		catch(InputMismatchException e) {
-			System.out.println("java.util.InputMismatchException");
+		catch(InputMismatchException e) {//if x or y is not integer, implement this 
+			System.out.println("java.util.InputMismatchException");//print out error message
 		}
-		catch(Exception e) {
-			System.out.println("Some other exception has occurred: "+e.getMessage());
+		catch(Exception e) {//etc error
+			System.out.println("Some other exception has occurred: "+e.getMessage());//print out error message with Exception String
 		}
-		
+
 	}
 
 }
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/Inexcep_main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/Inexcep_main.java
@@ -5,7 +5,7 @@ public class Inexcep_main {//class name
 	public static void main(String[] args) {//main method
 		int x,y;//declare variable x,y
 		In_exception obj = new In_exception();//instantiation In_ception class
-		obj.distinguishError();
+		obj.distinguishError();//call distinguishError method
 	}
 
 }
