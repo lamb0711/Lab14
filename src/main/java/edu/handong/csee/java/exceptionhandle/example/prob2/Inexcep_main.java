32899e3723a43e823ef3bc68344baeef0b6d08cd
Yang sujin <21500394@handong.edu>
2018-05-10 01:11:22 +0900

    declare variable and instantiation In_ception class

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/Inexcep_main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob2/Inexcep_main.java
@@ -1,9 +1,10 @@
-package edu.handong.csee.java.exceptionhandle.example.prob2;
+package edu.handong.csee.java.exceptionhandle.example.prob2;//package name
 
-public class Inexcep_main {
+public class Inexcep_main {//class name
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {//main method
+		int x,y;//declare variable x,y
+		In_exception obj = new In_exception();//instantiation In_ception class
 
 	}
 
