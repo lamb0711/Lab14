136f7c11cdface5d767d7ef655dc0182e7f65081
Yang sujin <21500394@handong.edu>
2018-05-10 14:52:38 +0900

    add line comments

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerCalculate.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerCalculate.java
@@ -1,18 +1,18 @@
-package edu.handong.csee.java.exceptionhandle.example.prob3;
+package edu.handong.csee.java.exceptionhandle.example.prob3;//package name
 
-public class PowerCalculate {
+public class PowerCalculate {//PowerCalculae class. it calculate n and p to n^p
 	
-	public long calculateNumber(int n, int p) throws Exception{
-		long result = 0;
+	public long calculateNumber(int n, int p) throws Exception{//calculate method return long type. it include Exception class
+		long result = 0;//declare long type variable
 		
-		result = (long)Math.pow(n, p);
-		if(n<0 || p < 0) {
-			throw new Exception("n or p should not be negative. ");
+		result = (long)Math.pow(n, p);//save calculate result to result into long type
+		if(n<0 || p < 0) {//if n and p are smaller than 0
+			throw new Exception("n or p should not be negative. ");//throw this message to catch
 		}
-		if(n == 0 && p==0) {
-			throw new Exception("n and p should not be zero. ");
+		if(n == 0 && p==0) {//if n and p are same as 0
+			throw new Exception("n and p should not be zero. ");//throw this message to catch
 		}
-		return result;
+		return result;//return long type variable result
 	}
 
 }
