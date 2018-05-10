5a795dbb64b97760584e0cab695bc142bb724844
Yang sujin <21500394@handong.edu>
2018-05-10 14:53:21 +0900

    arrange Prob3

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerCalculate.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerCalculate.java
@@ -1,10 +1,10 @@
 package edu.handong.csee.java.exceptionhandle.example.prob3;//package name
 
 public class PowerCalculate {//PowerCalculae class. it calculate n and p to n^p
-	
+
 	public long calculateNumber(int n, int p) throws Exception{//calculate method return long type. it include Exception class
 		long result = 0;//declare long type variable
-		
+
 		result = (long)Math.pow(n, p);//save calculate result to result into long type
 		if(n<0 || p < 0) {//if n and p are smaller than 0
 			throw new Exception("n or p should not be negative. ");//throw this message to catch
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerMain.java
@@ -4,13 +4,13 @@ import java.util.Scanner;//use scanner class
 
 public class PowerMain {//main class
 	public static final PowerCalculate myCalculator = new PowerCalculate();//declare to final variable. instantiation PowerCalculate to myClaculator
-    public static final Scanner in = new Scanner(System.in);//use Scanner class
-    
+	public static final Scanner in = new Scanner(System.in);//use Scanner class
+
 	public static void main(String[] args) {//main method
 		while(in.hasNextInt()) {//until scanner close, save input data to n and p
 			int n = in.nextInt();//save input integer to n
 			int p = in.nextInt();//save input integer to p
-			
+
 			try {//if it cause Exception, go to catch 
 				System.out.println(myCalculator.calculateNumber(n,p));//print result that called calcuateNumber method
 			}
@@ -18,7 +18,7 @@ public class PowerMain {//main class
 				System.out.println(e);//print Exception variable
 			}
 		}
-		
+
 
 	}
 
