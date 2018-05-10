c170c88f038af3dce3e035be6f6ad98963526a60
Yang sujin <21500394@handong.edu>
2018-05-10 21:51:29 +0900

    add line comments and arragne it

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomMain.java
@@ -1,38 +1,38 @@
-package edu.handong.csee.java.exceptionhandle.example.prob6;
+package edu.handong.csee.java.exceptionhandle.example.prob6;//package name
 
-import java.util.Scanner;
+import java.util.Scanner;//use scanner class
 
-public class CustomMain {
+public class CustomMain {//Main class
 
-	public static void main(String[] args) {
-		String name, race;
-		int age = 0;
-		Scanner keyboard = new Scanner(System.in);
-		CustomID obj = new CustomID();
-		boolean repeat = true;
-		
-		while(repeat) {
-			try {
-				if(obj.getState() == 0) {
-					System.out.print("Enter your name: ");
-					name = keyboard.nextLine();
-					obj.setName(name);
-				}else if(obj.getState() == 1) {
-					System.out.print("Enter your age: ");
-					age = keyboard.nextInt();
-					keyboard.nextLine();
-					obj.setAge(age);
-				}else if(obj.getState() == 2) {
-					System.out.print("Enter your race: ");
-					race = keyboard.nextLine();
-					obj.setRace(race);
+	public static void main(String[] args) {//main method
+		String name, race;//declare name and race
+		int age = 0;//declare age integer variable
+		Scanner keyboard = new Scanner(System.in);//instantiation scanner class to use keyboard
+		CustomID obj = new CustomID();//instantiation CustomID class to obj
+		boolean repeat = true;//save boolean variable. it help loop
+
+		while(repeat) {//loop infinite
+			try {//if it cause Exception, go to catch
+				if(obj.getState() == 0) {//call getState method to distinguish state number. if state is 0 
+					System.out.print("Enter your name: ");//print message to input data
+					name = keyboard.nextLine();//save input data to name
+					obj.setName(name);//call setName method
+				}else if(obj.getState() == 1) {//call getState method to distinguish state number. if state is 1
+					System.out.print("Enter your age: ");//print message to input data
+					age = keyboard.nextInt();//save input data to age
+					keyboard.nextLine();//for empty space
+					obj.setAge(age);//call setAge method
+				}else if(obj.getState() == 2) {//call getState method to distinguish state number. if state is 2
+					System.out.print("Enter your race: ");//print message to input data
+					race = keyboard.nextLine();//save input data to race
+					obj.setRace(race);//call setRace method
 				}
-			}catch(Exception e) {
-				System.out.println(e.getMessage());
-				continue;
+			}catch(Exception e) {//if it cause Exception. implement this
+				System.out.println(e.getMessage());//print error message in e
+				continue;//repeat loop
 			}
 		}
-		
+
 	}
 
 }
