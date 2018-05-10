7e1add0914c47e37731be685d8c07ab5948fae55
Yang sujin <21500394@handong.edu>
2018-05-10 12:47:41 +0900

    add method and line comments

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowerMain.java
@@ -1,9 +1,24 @@
-package edu.handong.csee.java.exceptionhandle.example.prob3;
+package edu.handong.csee.java.exceptionhandle.example.prob3;//package name
 
-public class PowerMain {
+import java.util.Scanner;//use scanner class
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class PowerMain {//main class
+	public static final PowerCalculate myCalculator = new PowerCalculate();//declare to final variable. instantiation PowerCalculate to myClaculator
+    public static final Scanner in = new Scanner(System.in);//use Scanner class
+    
+	public static void main(String[] args) {//main method
+		while(in.hasNextInt()) {//until scanner close, save input data to n and p
+			int n = in.nextInt();//save input integer to n
+			int p = in.nextInt();//save input integer to p
+			
+			try {//if it cause Exception, go to catch 
+				System.out.println(myCalculator.calculateNumber(n,p));//print result that called calcuateNumber method
+			}
+			catch(Exception e) {//if cause Exception, implement it
+				System.out.println(e);//print Exception variable
+			}
+		}
+		
 
 	}
 
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/Powercalc.java
+++
@@ -1,5 +0,0 @@
-package edu.handong.csee.java.exceptionhandle.example.prob3;
-
-public class Powercalc {
-
-}
