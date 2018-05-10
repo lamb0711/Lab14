c83ef66fa4dbf77ee622d196cbb952900a9fefd3
Yang sujin <21500394@handong.edu>
2018-05-10 15:42:45 +0900

    add line comments and arrange prob4

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/BoundMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/BoundMain.java
@@ -1,23 +1,23 @@
-package edu.handong.csee.java.exceptionhandle.example.prob4;
+package edu.handong.csee.java.exceptionhandle.example.prob4;//pakcage name
 
-import java.util.Scanner;
+import java.util.Scanner;//use scanner class
 
-public class BoundMain {
+public class BoundMain {//Main class
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		int num = 0;
-		Boundary arr = new Boundary();
-		boolean repeat = true;
-		while(repeat) {
-			try {
-				System.out.print("Enter an integer: ");
-				num = keyboard.nextInt();
-				arr.receiveNumber(num);
+	public static void main(String[] args) {//main method
+		Scanner keyboard = new Scanner(System.in);//use scanner class to use keyboard
+		int num = 0;//save input data variable
+		Boundary arr = new Boundary();//instantiation Boundary class to arr
+		boolean repeat = true;//help loop variable
+		while(repeat) {//if repeat is true, implement.
+			try {//if it cause Exception, go to catch
+				System.out.print("Enter an integer: ");//print message for input data
+				num = keyboard.nextInt();//save input data to num
+				arr.receiveNumber(num);//call recieveNumber method and distinguish Exception of existence
 			}
-			catch( ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid array index access!");
-				repeat = false;
+			catch( ArrayIndexOutOfBoundsException e) {//if cause Exception that over array index, implement it
+				System.out.println("Invalid array index access!");//print error message
+				repeat = false;//save false to repeat, so it stop the loop
 			}
 		}
 
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/Boundary.java
@@ -1,13 +1,13 @@
-package edu.handong.csee.java.exceptionhandle.example.prob4;
+package edu.handong.csee.java.exceptionhandle.example.prob4;//package name
 
-public class Boundary {
-	private static int [] arr = new int[5];
-	private static int cnt = 0;
-	public void receiveNumber(int num) throws ArrayIndexOutOfBoundsException{
-		int i = cnt;
-		arr[i] = num;
-		cnt++;
-		System.out.printf("arr[%d] <- %d\n",i,num);
+public class Boundary {//Boundary class. it save input data to array on 5times
+	private static int [] arr = new int[5];//declare array arr[5]
+	private static int cnt = 0;//this variable help to loop
+	public void receiveNumber(int num) throws ArrayIndexOutOfBoundsException{//receiceNumber method receive input data and if it cause error(like over array size) throw catch
+		int i = cnt;//save cnt number to i. i is array index
+		arr[i] = num;//save input data num to arr[i]
+		cnt++;//add loop number
+		System.out.printf("arr[%d] <- %d\n",i,num);//print input array data and array index
 	}
 
 }
