1556b81e971594aa7aae12a5c78c8e9a4773b4bd
Yang sujin <21500394@handong.edu>
2018-05-10 20:34:22 +0900

    add line comments

---
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob5/MyException.java
@@ -0,0 +1,23 @@
+package edu.handong.csee.java.exceptionhandle.example.prob5;//package name
+
+public class MyException extends Exception {//MyException class it inherit Exception class. And this class use overriding
+	private String message = null;//save string variable.
+	
+	public MyException() {//if it do not have any parameter, implement this constructor 
+		super();//call super method in Exception.
+	}
+	
+	public MyException(String message) {//if it have string parameter, implement this constructor 
+		super(message);//call super method with message string
+		this.message = message;//save input data to message
+	}
+	
+	public MyException(Throwable cause) {//if any error cause, implement this constructor 
+		super(cause);//throw error message
+	}
+	
+	public String toString() {//toStrign class return string
+		return message;//return message
+	}
+
+}
---
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob5/MyMain.java
@@ -0,0 +1,24 @@
+package edu.handong.csee.java.exceptionhandle.example.prob5;//package name
+
+import java.util.Scanner;//use scanner class
+
+public class MyMain {//Main class
+	public static void main(String[] a) {//main method
+		try {//if it cause error, go to catch
+			Scanner keyboard = new Scanner(System.in);//instantiation scanner class to use keyboard
+			String str = keyboard.nextLine();//declare string str and save input string
+			MyMain.testString(str);//call testString method with str
+		}catch(MyException mae) {//receive error string mae and implement catch
+			System.out.println("Inside catch block: "+mae);//print out message with error message mae
+		}
+	}
+
+	private static void testString(String str) throws MyException{//testString method test input string, it include to Myexception exception
+		if(str.equals("null")) {//if string is same as null
+			throw new MyException("String val is null");//instantiation MyException class and throw this message to catch
+		}
+		else System.out.println("String val is: "+str);//if string is different to null, print message with str
+		
+	}
+
+}
